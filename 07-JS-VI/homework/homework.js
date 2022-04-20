fe3dbcb1540328302f54d591e210cc9e51af343d
Eduardo9324 <cris122493@gmail.com>
2022-04-19 20:39:33 -0500

    MODULOS JS COMPLETOS

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,22 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  const nnm = nombre[0].toUpperCase() + nombre.slice(1);
+  return nnm;
+  
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,26 +27,40 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let sumaNu = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    sumaNu += numeros[i];
+  }
+  cb(sumaNu);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  let resultados = array.map(cb);
 }
+
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  const narr = array.map(cb);
+  return narr;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  const filtros = array.filter(function (str) {
+    return str[0] == "a";
+  });
+  return filtros;
 }
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
